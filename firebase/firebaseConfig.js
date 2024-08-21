7738a6b547f17cea0962da88d12d6e1b05e53c01
Rohan Shah <rohanshah.13@hotmail.co.uk>
2024-08-21 23:44:27 +0100

    environment variables

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,17 +1,16 @@
-// firebase/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuItZ5TkwrSqROnpJ2XJM-uPMivQIsAYo",
-  authDomain: "gymprover-2a47f.firebaseapp.com",
-  databaseURL: "https://gymprover-2a47f-default-rtdb.firebaseio.com/",
-  projectId: "gymprover-2a47f",
-  storageBucket: "gymprover-2a47f.appspot.com",
-  messagingSenderId: "482541159483",
-  appId: "1:482541159483:web:66b021cbe7e3e11219881e",
-  measurementId: "G-S9MDF9VL17",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
